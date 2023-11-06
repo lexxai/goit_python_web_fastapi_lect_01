7157a24ed6efb3017733d702abdbc013dfed98b0
lexxai <lexxai@users.noreply.github.com>
2023-11-06 03:28:15 +0200

    repository owners

---
+++ goit_python_web_fastapi_lect_01/src/repository/owners.py
@@ -0,0 +1,46 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+
+from src.shemas import OwnerModel
+from src.database.models import Owner
+
+
+
+async def get_owners(db: Session):
+    owners = db.query(Owner).all()
+    return owners
+
+
+async def get_owner_by_id(owner_id: int, db: Session):
+    owner = db.query(Owner).filter_by(id=owner_id).first()
+    return owner
+
+
+async def get_owner_by_email(email: str, db: Session):
+    owner = db.query(Owner).filter_by(email=email).first()
+    return owner
+
+
+async def create_owner(body: OwnerModel, db: Session):
+    owner = Owner(**body.model_dump())
+    db.add(owner)
+    db.commit()
+    db.refresh(owner)
+    return owner
+
+
+async def update_owner(owner_id: int, body: OwnerModel, db: Session):
+    owner = await get_owner_by_id(owner_id, db)
+    if owner:
+        owner.email = body.email
+        db.commit()
+    return owner
+
+
+async def delete_owner(owner_id: int, db: Session):
+    owner = await get_owner_by_id(owner_id, db)
+    if owner:
+        db.delete(owner)
+        db.commit()
+    return owner
+        
--- goit_python_web_fastapi_lect_01/src/routes/owners.py
+++ goit_python_web_fastapi_lect_01/src/routes/owners.py
@@ -2,11 +2,12 @@ from typing import List
 
 from fastapi import Path,  Depends, HTTPException, status, APIRouter
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 
 from src.database.db import get_db
 from src.shemas import  OwnerModel, OwnerResponse
-from src.database.models import Owner
-from sqlalchemy.orm import Session
+from src.repository import owners as repository_owners
+
 
 router = APIRouter(prefix="/owners",  tags=["owners"])
 
@@ -16,13 +17,14 @@ router = APIRouter(prefix="/owners",  tags=["owners"])
     response_model=List[OwnerResponse]
 )
 async def get_owners(db: Session = Depends(get_db)):
-    owners = db.query(Owner).all()
+    owners = await repository_owners.get_owners(db)
     return owners
+
 
 
 @router.get("/{owner_id}", response_model=OwnerResponse)
 async def get_owner(owner_id: int = Path(ge=1), db: Session = Depends(get_db)):
-    owner = db.query(Owner).filter_by(id=owner_id).first()
+    owner = await repository_owners.get_owner_by_id(owner_id, db)
     if owner is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
     return owner
@@ -30,16 +32,13 @@ async def get_owner(owner_id: int = Path(ge=1), db: Session = Depends(get_db)):
 
 @router.post("/", response_model=OwnerResponse, status_code=status.HTTP_201_CREATED)
 async def create_owner(body: OwnerModel, db: Session = Depends(get_db)):
-    owner = db.query(Owner).filter_by(email=body.email).first()
+    owner = await repository_owners.get_owner_by_email(body.email, db)
     if owner:
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT, detail=f"Email is exist!"
         )
     try:
-        owner = Owner(**body.model_dump())
-        db.add(owner)
-        db.commit()
-        db.refresh(owner)
+        owner = await repository_owners.create_owner(body, db)
     except IntegrityError as err:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Error: {err}"
@@ -51,11 +50,9 @@ async def create_owner(body: OwnerModel, db: Session = Depends(get_db)):
 async def update_owner(
     body: OwnerModel, owner_id: int = Path(ge=1), db: Session = Depends(get_db)
 ):
-    owner = db.query(Owner).filter_by(id=owner_id).first()
+    owner = await repository_owners.update_owner(owner_id, body, db)
     if owner is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
-    owner.email = body.email
-    db.commit()
     return owner
 
 
@@ -63,9 +60,7 @@ async def update_owner(
     "/{owner_id}", status_code=status.HTTP_204_NO_CONTENT
 )
 async def remove_owner(owner_id: int = Path(ge=1), db: Session = Depends(get_db)):
-    owner = db.query(Owner).filter_by(id=owner_id).first()
+    owner = await repository_owners.delete_owner(owner_id, db)
     if owner is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
-    db.delete(owner)
-    db.commit()
     return None
