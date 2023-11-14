976ff3bc92f66a8f70889ef71e6219f8dd3143ae
lexxai <lexxai@users.noreply.github.com>
2023-11-14 03:21:29 +0200

    get_refresh_token

--- goit_python_web_fastapi_lect_01/static/client/js/app.js
+++ goit_python_web_fastapi_lect_01/static/client/js/app.js
@@ -1,13 +1,51 @@
-const token = localStorage.getItem("access_token");
+let token = localStorage.getItem("access_token");
+
 const BASE_URL = "http://localhost:9000";
 
-console.log(`token=${token}`);
+//console.log(`token=${token}`);
 
 function setLoading(target) {
-    target.innerHTML='<div class="alert alert-primary" role="alert">Loading...</div>'
+  target.innerHTML =
+    '<div class="alert alert-primary" role="alert">Loading...</div>';
 }
 
+get_refresh_token = async () => {
+  get_refresh_token.counter = (get_refresh_token.counter || 0) + 1;
+  if (get_refresh_token > 1) {
+    console.log("get_refresh_token is busy, try later");
+    return;
+  }
+  const URL = `${BASE_URL}/api/auth/refresh_token`;
+  let refresh_token = localStorage.getItem("refresh_token");
+  console.log(`refresh_token. token=${token}, refresh_token=${refresh_token}`);
+
+  const response = await fetch(URL, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${refresh_token}`,
+    },
+  });
+
+  if (response.ok) {
+    result = await response.json();
+    localStorage.setItem("access_token", result?.access_token);
+    localStorage.setItem("refresh_token", result?.refresh_token);
+    token = localStorage.getItem("access_token");
+    // refresh_token = localStorage.getItem("refresh_token");
+    get_refresh_token.counter = 0;
+    return token;
+  } else if (response.status == 401) {
+    //window.location = "index.html";
+    get_refresh_token.counter = 0;
+  } else {
+    get_refresh_token.counter = 0;
+  }
+  get_refresh_token.counter = 0;
+};
+
 get_cats = async () => {
+  get_cats.counter = (get_cats.counter || 0) + 1;
+  setLoading(cats);
   const URL = `${BASE_URL}/api/cats`;
   const response = await fetch(URL, {
     method: "GET",
@@ -16,6 +54,7 @@ get_cats = async () => {
     },
   });
   if (response.ok) {
+    get_cats.counter = 0;
     cats.innerHTML = "";
     result = await response.json();
     for (cat of result) {
@@ -24,12 +63,20 @@ get_cats = async () => {
       el.innerHTML = `ID: ${cat?.id} Name: <strong>${cat?.nickname}</strong> Status: ${cat?.vaccinated} Owner: ${cat?.owner.email}`;
       cats.appendChild(el);
     }
-  }else if  (response.status == 401) {
-    window.location = "index.html"
+  } else if (response.status == 401) {
+    if (get_cats.counter < 4) {
+      console.log(`Try: refresh_token counter: ${get_cats.counter}`);
+      token = await get_refresh_token();
+      setTimeout(get_cats, 3000);
+    } else {
+      console.log(`Try: to login page counter: ${get_cats.counter}`);
+      window.location = "index.html";
+    }
   }
 };
 
 get_owners = async () => {
+  get_owners.counter = (get_owners.counter || 0) + 1;
   setLoading(owners);
   const URL = `${BASE_URL}/api/owners`;
   const response = await fetch(URL, {
@@ -39,6 +86,7 @@ get_owners = async () => {
     },
   });
   if (response.ok) {
+    get_owners.counter = 0;
     owners.innerHTML = "";
     result = await response.json();
     for (owner of result) {
@@ -47,8 +95,14 @@ get_owners = async () => {
       el.innerHTML = `ID: ${owner?.id} Email: <strong>${owner?.email}</strong>`;
       owners.appendChild(el);
     }
-  }else if  (response.status == 401) {
-    window.location = "index.html"
+  } else if (response.status == 401) {
+    if (get_owners.counter < 4) {
+      console.log(`Try: refresh_token counter: ${get_owners.counter}`);
+      token = await get_refresh_token();
+      setTimeout(get_owners, 3000);
+    } else {
+      window.location = "index.html";
+    }
   }
 };
 
@@ -69,10 +123,18 @@ ownerCreate.addEventListener("submit", async (e) => {
   if (response.status == 201) {
     ownerCreate.reset();
     get_owners();
-  }else if  (response.status == 401) {
-    window.location = "index.html"
+  } else if (response.status == 401) {
+    token = await get_refresh_token();
+    console.log("Try again");
+  } else {
+    window.location = "index.html";
   }
 });
 
 get_cats();
 get_owners();
+
+setTimeout(() => {
+  get_cats();
+}, 15000);
+// setTimeout(()=>{get_cats},15000);
